85b82a4ee39235daafcfe72710fc8a0078859dbc
ty-strong <sk8moola@gmail.com>
2025-04-24 11:41:32 -0700

    Player can move in any direction, and cleaned up the code in main.py

--- main.py
+++ main.py
@@ -9,19 +9,33 @@ def main():
     print("Starting Asteroids!")
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
+
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
+        
+        for object in drawable:
+            object.draw(screen)
+        
         pygame.display.flip()
+        
+        # set to 60fps
         dt = clock.tick(60) / 1000
-        player.update(dt)
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -25,7 +25,15 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
